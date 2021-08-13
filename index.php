9ba1caff702030f2582389fc1a5b5e4eef9c19c8
Yasitha Ranga <yasitharanga1@gmail.com>
2021-08-14 00:12:21 +0530

    Updated

--- index.php
+++ index.php
@@ -2,21 +2,16 @@
 <html>
 <head>
 <title>Home | Abditory</title>
-	<!-- this page stylesheet --
-	<link rel="stylesheet" type="text/css" href="">
-	>
-	<!-------------- ---------------------->
-	
 	
 	<!--- php call for globalHeader.html----->
 	<?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/assets/"; include($IPATH."globalHeader.html"); ?>
 	<!-- Page Content --->
 <div class="content">
-  <h1>Welcome, Abditory Picks for you</h1>
+  <h1 id="mainTitle">Welcome, Abditory Picks for you</h1>
 	<?php include($IPATH."abditoryPicks.php"); ?>		
 	
 	
-  <h1>Most Popular</h1>
+  <h1 id="mainTitle">Most Popular</h1>
 	<?php include($IPATH."MostPopular.php"); ?>	
 </div>
 
