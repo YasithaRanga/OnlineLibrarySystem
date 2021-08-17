d829ec9e4a03fa730232c7a9d1d9201996efe67a
Yasitha Ranga <yasitharanga1@gmail.com>
2021-08-17 05:57:10 +0530

    Updated

--- categories.php
+++ categories.php
@@ -1,10 +1,15 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<title>Abditory | Categories</title>
 
-<body>
-</body>
+<!--- php call for globalHeader.html----->
+<?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/assets/"; include($IPATH."globalHeader.php"); ?>
+	
+	
+
+	
+<!--- php call for globalFooter.html----->
+<?php include($IPATH."globalFooter.html"); ?>
+
 </html>
