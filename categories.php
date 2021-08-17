d7905fe6c0767917687143a270ea1f03e9f25292
Yasitha Ranga <yasitharanga1@gmail.com>
2021-08-17 05:57:39 +0530

    Added Categories

--- categories.php
+++ categories.php
@@ -6,7 +6,10 @@
 <!--- php call for globalHeader.html----->
 <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/assets/"; include($IPATH."globalHeader.php"); ?>
 	
-	
+<div class="section" id="section2">
+  		<h1 id="mainTitle">Most Popular</h1>
+		<?php include($IPATH."MostPopular.php"); ?>
+</div>
 
 	
 <!--- php call for globalFooter.html----->
